903f320b04f093abd86a701fcc1b220418281dbd
FrancoNordinelli <imnordi@gmail.com>
2024-03-27 19:21:23 -0300

    1.10 Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react"
 
+const StatisticsLine = ({ text, value }) => {
 
-const Statistics = (props) => {
-
-  if(props.bad === 0 && props.good === 0 && props.neutral === 0){
-    return(
-      <h1>No feedback given</h1>
-    )
-  }
 
   return (
+    
+          <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+          </tr>
+        
+  )
+}
+
+const Statistics = (props) => {
+  const total = props.good + props.bad + props.neutral
+  if (total === 0) {
+    return <h1>No feedback given</h1>
+  }
+
+  const godBad = props.godBad + props.bad
+  return (
     <div>
-      <h1>Statistics</h1>
-      <h4 style={{ margin: '0' }}>good {props.good}</h4>
-      <h4 style={{ margin: '0' }}>neutral {props.neutral}</h4>
-      <h4 style={{ margin: '0' }}>bad {props.bad} </h4>
-      <p style={{ margin: '0' }}>all {props.good + props.bad + props.neutral}</p>
-      <p style={{ margin: '0' }}>avarage {((props.good + props.bad * -1) / (props.good + props.bad + props.neutral))}</p>
-      <p style={{ margin: '0' }}>positive {(props.good)*100 / ( props.good + props.bad + props.neutral)} %</p>
+      <table>
+        <tbody>
+      <StatisticsLine text="good" value={props.good} />
+      <StatisticsLine text="neutral" value={props.neutral} />
+      <StatisticsLine text="bad" value={props.bad} />
+      <StatisticsLine text="all" value={total} />
+      <StatisticsLine text="avarage" value={(props.good + (props.bad * (-1))) / total} />
+      <StatisticsLine text="positive" value={(props.good / total) * 100 + ' ' + '%'} />
+      </tbody>
+      </table>
     </div>
   )
 }
+
+
 
 const Button = (props) => {
   return (
@@ -34,7 +50,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
 
   return (
     <div>
@@ -42,7 +57,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div >
   )
 }
