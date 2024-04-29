3d93118126ca8423792662b64f86b2825d35b2a4
FrancoNordinelli <imnordi@gmail.com>
2024-04-29 15:44:35 -0300

    exercises

---
+++ part2/course_info_data/src/Course.jsx
@@ -0,0 +1,35 @@
+import Header from './Header'
+
+const Course = () => {
+    // const course = {
+    //     id: 1,
+    //     name: 'Half Stack application development',
+    //     parts: [
+    //       {
+    //         name: 'Fundamentals of React',
+    //         exercises: 10,
+    //         id: 1
+    //       },
+    //       {
+    //         name: 'Using props to pass data',
+    //         exercises: 7,
+    //         id: 2
+    //       },
+    //       {
+    //         name: 'State of a component',
+    //         exercises: 14,
+    //         id: 3
+    //       }
+    //     ]
+    //   }
+
+    //  return "Hola"
+
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
+
+export default Course
---
+++ part2/course_info_data/src/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+
+    const header = {
+        id : 1,
+        name : 'Half Stack application development'
+    }
+
+    return (
+        <>
+        {header.name}
+        </>
+    )
+}
+
+export default Header
