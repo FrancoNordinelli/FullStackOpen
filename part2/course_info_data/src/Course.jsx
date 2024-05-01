62a56798ec7ff521e06bc03c1a37ee27faee3153
FrancoNordinelli <imnordi@gmail.com>
2024-05-01 11:33:44 -0300

    2.1

--- part2/course_info_data/src/Course.jsx
+++ part2/course_info_data/src/Course.jsx
@@ -2,36 +2,83 @@ import Content from './Content'
 import Header from './Header'
 
 const Course = () => {
-    // const course = {
-    //     id: 1,
-    //     name: 'Half Stack application development',
-    //     parts: [
-    //       {
-    //         name: 'Fundamentals of React',
-    //         exercises: 10,
-    //         id: 1
-    //       },
-    //       {
-    //         name: 'Using props to pass data',
-    //         exercises: 7,
-    //         id: 2
-    //       },
-    //       {
-    //         name: 'State of a component',
-    //         exercises: 14,
-    //         id: 3
-    //       }
-    //     ]
-    //   }
+    //COURSES ARRAY
+    const courses = [
+        //CONTEST COURSES. HALF STACK APPLICATION DEVELOPMENT, NODE JS
+        {
+            name: 'Half Stack Application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-    //  return "Hola"
+
+    const CourseItem = ({ course }) => {
+        return (
+            <div>
+                <h2>{course.name}</h2>
+                <div>
+                    {course.parts.map(part => (
+                        <Part key={part.id} part={part} />
+                    ))}
+                </div>
+            </div>
+        );
+    };
+
+    const Part = ({ part }) => {
+        return (
+            <div>
+                <p>{part.name}</p>
+            </div>
+        );
+    };
 
     return (
+        
         <div>
-            <Header></Header>
-            <p></p>
-            <Content></Content>
+            {courses.map(course => (
+                <CourseItem key={course.id} course={course} />
+            ))}
         </div>
+    
     )
 }
 
