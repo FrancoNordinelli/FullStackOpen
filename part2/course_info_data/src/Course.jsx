23b8925c6ecc85604d49a60fec384b7e1d4c3b83
FrancoNordinelli <imnordi@gmail.com>
2024-05-03 15:48:14 -0300

    fix 2.1

--- part2/course_info_data/src/Course.jsx
+++ part2/course_info_data/src/Course.jsx
@@ -1,85 +1,45 @@
-import Content from './Content'
-import Header from './Header'
 
-const Course = () => {
-    //COURSES ARRAY
-    const courses = [
-        //CONTEST COURSES. HALF STACK APPLICATION DEVELOPMENT, NODE JS
-        {
-            name: 'Half Stack Application development',
-            id: 1,
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1
-                },
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2
-                },
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3
-                },
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4
-                }
-            ]
-        },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                    name: 'Routing',
-                    exercises: 3,
-                    id: 1
-                },
-                {
-                    name: 'Middlewares',
-                    exercises: 7,
-                    id: 2
-                }
-            ]
-        }
-    ]
+const Header = ({ name }) => {
+    return <h1>{name}</h1>;
+  }
+  
 
-
-    const CourseItem = ({ course }) => {
-        return (
-            <div>
-                <h2>{course.name}</h2>
-                <div>
-                    {course.parts.map(part => (
-                        <Part key={part.id} part={part} />
-                    ))}
-                </div>
-            </div>
-        );
-    };
-
-    const Part = ({ part }) => {
-        return (
-            <div>
-                <p>{part.name}</p>
-            </div>
-        );
-    };
-
+const Content = ({ parts }) => {
     return (
-        
         <div>
-            {courses.map(course => (
-                <CourseItem key={course.id} course={course} />
+            {parts.map(part => (
+                <Part key={part.id} name={part.name} exercises={part.exercises} />
             ))}
         </div>
-    
     )
 }
 
+const Part = ({ name, exercises }) => {
+    return (
+        <p>
+            {name} {exercises} 
+        </p>
+    )
+}
+
+const Total = ({exercises}) =>{
+
+    const total = exercises.reduce((acum, currentValue) => {
+        return acum+currentValue.exercises
+    }, 0)
+    return(
+        <p>
+            total of {total} exercises
+        </p>
+    )
+}
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total exercises={course.parts}/>
+        </div>
+    )
+}
 export default Course
